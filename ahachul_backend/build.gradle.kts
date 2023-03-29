2d698183cda43d2e20b2373347c2e306a9ff0a8b
Joohyun Ha <mylifeforcoding@gmail.com>
2023-03-30 03:54:43 +0900

    :closed_lock_with_key: restdocs 설정 (#6)

--- ahachul_backend/build.gradle.kts
+++ ahachul_backend/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.4"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
@@ -16,6 +17,8 @@ repositories {
     mavenCentral()
 }
 
+val asciidoctorExt: Configuration by configurations.creating
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -23,6 +26,12 @@ dependencies {
 
     // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging/3.0.5
     implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+    // https://mvnrepository.com/artifact/org.springframework.restdocs/spring-restdocs-mockmvc/3.0.0
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    // https://docs.spring.io/spring-restdocs/docs/current/reference/htmlsingle/
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
@@ -38,4 +47,43 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks {
+    val snippetsDir = file("build/generated-snippets")
+
+    test {
+        outputs.dir(snippetsDir)
+        useJUnitPlatform()
+    }
+
+    asciidoctor {
+        configurations(asciidoctorExt.name)
+
+        doFirst {
+            delete("src/main/resources/static/docs")
+        }
+
+        forkOptions {
+            jvmArgs("--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED", "--add-opens", "java.base/java.io=ALL-UNNAMED")
+        }
+
+        inputs.dir(snippetsDir)
+
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("src/main/resources/static/docs")
+            }
+        }
+
+        sources {
+            include("**/index.adoc")
+        }
+        dependsOn(test)
+    }
+
+    build {
+        dependsOn(asciidoctor)
+    }
 }
