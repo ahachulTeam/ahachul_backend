76a08415a30aa776d564ca5aab3a09fb2c942743
Joohyun Ha <mylifeforcoding@gmail.com>
2023-03-29 22:59:58 +0900

    :sparkles: Exception 계층별로 분리 (#6)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/exception/CommonExceptionHandler.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/exception/CommonExceptionHandler.kt
@@ -1,12 +1,11 @@
 package backend.team.ahachul_backend.common.exception
 
 import backend.team.ahachul_backend.common.logging.Logger
-import backend.team.ahachul_backend.common.response.CommonException
 import backend.team.ahachul_backend.common.response.CommonResponse
 import backend.team.ahachul_backend.common.response.ResponseCode
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
@@ -15,24 +14,52 @@ class CommonExceptionHandler {
     val logger = Logger(javaClass)
 
     @ExceptionHandler(Exception::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun exception(e: Exception): CommonResponse<Unit> {
+    fun exception(e: Exception): ResponseEntity<CommonResponse<Unit>> {
         logger.error(
                 message = e.message,
                 code = ResponseCode.INTERNAL_SERVER_ERROR,
                 stackTrace = e.stackTrace.contentToString()
         )
-        return CommonResponse.fail()
+        return ResponseEntity(CommonResponse.fail(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(CommonException::class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun commonException(e: CommonException): CommonResponse<Unit> {
+    fun commonException(e: CommonException): ResponseEntity<CommonResponse<Unit>> {
         logger.error(
                 message = e.message,
                 code = e.code,
                 stackTrace = e.stackTrace.contentToString()
         )
-        return CommonResponse.fail(e.code)
+        return ResponseEntity(CommonResponse.fail(e.code), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(AdapterException::class)
+    fun commonException(e: AdapterException): ResponseEntity<CommonResponse<Unit>> {
+        logger.error(
+                message = e.message,
+                code = e.code,
+                stackTrace = e.stackTrace.contentToString()
+        )
+        return ResponseEntity(CommonResponse.fail(e.code), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(PortException::class)
+    fun commonException(e: PortException): ResponseEntity<CommonResponse<Unit>> {
+        logger.error(
+                message = e.message,
+                code = e.code,
+                stackTrace = e.stackTrace.contentToString()
+        )
+        return ResponseEntity(CommonResponse.fail(e.code), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(DomainException::class)
+    fun commonException(e: DomainException): ResponseEntity<CommonResponse<Unit>> {
+        logger.error(
+                message = e.message,
+                code = e.code,
+                stackTrace = e.stackTrace.contentToString()
+        )
+        return ResponseEntity(CommonResponse.fail(e.code), HttpStatus.BAD_REQUEST)
     }
 }
--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/response/CommonException.kt
+++
@@ -1,7 +0,0 @@
-package backend.team.ahachul_backend.common.response
-
-class CommonException(
-        val code: ResponseCode,
-        val errorMessage: String
-): RuntimeException(errorMessage) {
-}
