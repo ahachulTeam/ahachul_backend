2cd871439d66636f553ee0accdd14f9eab3a3605
Joohyun Ha <mylifeforcoding@gmail.com>
2023-04-06 00:36:00 +0900

    :sparkles: 사용자 정보 조회 API 구현 (#17)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/adapter/web/in/MemberController.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/adapter/web/in/MemberController.kt
@@ -1,11 +1,10 @@
 package backend.team.ahachul_backend.api.member.adapter.web.`in`
 
+import backend.team.ahachul_backend.api.member.adapter.web.`in`.dto.GetMemberDto
 import backend.team.ahachul_backend.api.member.application.port.`in`.MemberUseCase
 import backend.team.ahachul_backend.common.annotation.Authentication
 import backend.team.ahachul_backend.common.response.CommonResponse
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,14 +13,8 @@ class MemberController(
 ) {
 
     @Authentication
-    @GetMapping("/v1/members/{memberId}/is-add-profiles")
-    fun isAddProfiles(@PathVariable memberId: Long): CommonResponse<*>? {
-        return null
-    }
-
-    @Authentication
-    @PatchMapping("/v1/members/{memberId}")
-    fun updateMember(@PathVariable memberId: Long): CommonResponse<*>? {
-        return null
+    @GetMapping("/v1/members")
+    fun getMember(): CommonResponse<GetMemberDto.Response> {
+        return CommonResponse.success(memberUseCase.getMember())
     }
 }
