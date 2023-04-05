803172552681e2a5385d033714969ef7d4e3b865
Joohyun Ha <mylifeforcoding@gmail.com>
2023-04-05 23:25:07 +0900

    :sparkles: 권한 인터셉터 구현 (#18)

---
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/annotation/Authentication.kt
@@ -0,0 +1,5 @@
+package backend.team.ahachul_backend.common.annotation
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Authentication
--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/response/ResponseCode.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/response/ResponseCode.kt
@@ -10,6 +10,9 @@ enum class ResponseCode(
     SUCCESS("100", "SUCCESS", HttpStatus.OK),
     BAD_REQUEST("101", "BAD_REQUEST", HttpStatus.BAD_REQUEST),
     INTERNAL_SERVER_ERROR("102", "INTERNAL_SERVER_ERROR", HttpStatus.BAD_REQUEST),
+    INVALID_DOMAIN("103", "유효하지 않은 도메인입니다.", HttpStatus.BAD_REQUEST),
 
     INVALID_APPLE_ID_TOKEN("200", "유효하지 않은 ID 토큰입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ACCESS_TOKEN("201", "유효하지 않은 엑세스 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    EXPIRED_ACCESS_TOKEN("202", "유효기간이 만료된 엑세스 토큰입니다.", HttpStatus.UNAUTHORIZED),
 }
