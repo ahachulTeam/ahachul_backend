31541839aa7f278ae3d777f5a83520396b3060a1
Hyun <mylifeforcoding@gmail.com>
2023-04-06 16:36:56 +0900

    :sparkles: 사용자 정보 수정 API 구현 (#23) (#24)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/application/port/in/MemberUseCase.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/application/port/in/MemberUseCase.kt
@@ -1,8 +1,12 @@
 package backend.team.ahachul_backend.api.member.application.port.`in`
 
 import backend.team.ahachul_backend.api.member.adapter.web.`in`.dto.GetMemberDto
+import backend.team.ahachul_backend.api.member.adapter.web.`in`.dto.UpdateMemberDto
+import backend.team.ahachul_backend.api.member.application.port.`in`.command.UpdateMemberCommand
 
 interface MemberUseCase {
 
     fun getMember(): GetMemberDto.Response
+
+    fun updateMember(updateMemberCommand: UpdateMemberCommand): UpdateMemberDto.Response
 }
