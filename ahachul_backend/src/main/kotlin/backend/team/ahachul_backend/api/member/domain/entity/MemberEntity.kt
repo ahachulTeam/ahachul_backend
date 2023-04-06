2d681ebf6cef86a5cbbedf835571d1861e9a1723
semi-cloud <sksmsrkdtpal@naver.com>
2023-04-06 14:44:26 +0900

    :bug: 구글 소셜 로그인 요청 API 예외 해결 (#19) (#22)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/domain/entity/MemberEntity.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/domain/entity/MemberEntity.kt
@@ -3,7 +3,9 @@ package backend.team.ahachul_backend.api.member.domain.entity
 import backend.team.ahachul_backend.api.member.application.port.`in`.command.LoginMemberCommand
 import backend.team.ahachul_backend.api.member.domain.model.GenderType
 import backend.team.ahachul_backend.api.member.domain.model.ProviderType
+
 import backend.team.ahachul_backend.api.member.domain.model.MemberStatus
+import backend.team.ahachul_backend.common.dto.GoogleUserInfoDto
 import backend.team.ahachul_backend.common.dto.KakaoMemberInfoDto
 import backend.team.ahachul_backend.common.entity.BaseEntity
 import jakarta.persistence.*
@@ -34,7 +36,7 @@ class MemberEntity(
 ): BaseEntity() {
 
         companion object {
-                fun of(command: LoginMemberCommand, userInfo: KakaoMemberInfoDto): MemberEntity {
+                fun ofKakao(command: LoginMemberCommand, userInfo: KakaoMemberInfoDto): MemberEntity {
                         return MemberEntity(
                                 nickname = userInfo.kakaoAccount.profile?.nickname,
                                 providerUserId = userInfo.id,
@@ -42,6 +44,18 @@ class MemberEntity(
                                 email = userInfo.kakaoAccount.email,
                                 gender = userInfo.kakaoAccount.gender?.let { GenderType.of(it) },
                                 ageRange = userInfo.kakaoAccount.ageRange?.let { it.split("~")[0] },
+                                status = MemberStatus.ACTIVE
+                        )
+                }
+
+                fun ofGoogle(command: LoginMemberCommand, userInfo: GoogleUserInfoDto): MemberEntity {
+                        return MemberEntity(
+                                nickname = userInfo.name,
+                                providerUserId = userInfo.id,
+                                provider = command.providerType,
+                                email = userInfo.email,
+                                gender = null,
+                                ageRange = null,
                                 status = MemberStatus.ACTIVE
                         )
                 }
--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/dto/GoogleUserInfoDto.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/dto/GoogleUserInfoDto.kt
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class GoogleUserInfoDto (
     @JsonProperty("id") val id: String,
-    @JsonProperty("name") val name: String,
     @JsonProperty("email") val email: String,
-    @JsonProperty("verifiedEmail") val verifiedEmail: Boolean,
-    @JsonProperty("givenName") val givenName: String,
-    @JsonProperty("familyName") val familyName: String,
+    @JsonProperty("verified_email") val verifiedEmail: Boolean,
+    @JsonProperty("name") val name: String,
+    @JsonProperty("given_name") val givenName: String,
+    @JsonProperty("family_name") val familyName: String,
     @JsonProperty("picture") val picture: String,
     @JsonProperty("locale") val locale: String
 )
