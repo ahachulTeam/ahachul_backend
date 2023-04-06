4226e5972d93a559603adc932073ec8fc2108742
Joohyun Ha <mylifeforcoding@gmail.com>
2023-04-06 12:46:42 +0900

    :sparkles: 사용자 조회 API 구현 (#17) (#21)

---
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/adapter/web/in/dto/GetMemberDto.kt
@@ -0,0 +1,26 @@
+package backend.team.ahachul_backend.api.member.adapter.web.`in`.dto
+
+import backend.team.ahachul_backend.api.member.domain.entity.MemberEntity
+import backend.team.ahachul_backend.api.member.domain.model.GenderType
+
+class GetMemberDto {
+    data class Response(
+            val memberId: Long?,
+            val nickname: String?,
+            val email: String?,
+            val gender: GenderType?,
+            val ageRange: String?
+    ) {
+        companion object {
+            fun of(memberEntity: MemberEntity): Response {
+                return Response(
+                        memberId = memberEntity.id,
+                        nickname = memberEntity.nickname,
+                        email = memberEntity.email,
+                        gender = memberEntity.gender,
+                        ageRange = memberEntity.ageRange
+                )
+            }
+        }
+    }
+}
---
+++ ahachul_backend/src/test/kotlin/backend/team/ahachul_backend/api/member/application/service/MemberServiceTest.kt
@@ -0,0 +1,50 @@
+package backend.team.ahachul_backend.api.member.application.service
+
+import backend.team.ahachul_backend.api.member.adapter.web.out.MemberRepository
+import backend.team.ahachul_backend.api.member.application.port.`in`.MemberUseCase
+import backend.team.ahachul_backend.api.member.domain.entity.MemberEntity
+import backend.team.ahachul_backend.api.member.domain.model.GenderType
+import backend.team.ahachul_backend.api.member.domain.model.MemberStatus
+import backend.team.ahachul_backend.api.member.domain.model.ProviderType
+import backend.team.ahachul_backend.common.utils.RequestUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemberServiceTest(
+        @Autowired val memberRepository: MemberRepository,
+        @Autowired val memberUseCase: MemberUseCase
+) {
+
+    @BeforeEach
+    fun setup() {
+        val member = memberRepository.save(MemberEntity(
+                nickname = "nickname",
+                provider = ProviderType.KAKAO,
+                providerUserId = "providerUserId",
+                email = "email",
+                gender = GenderType.MALE,
+                ageRange = "20",
+                status = MemberStatus.ACTIVE
+        ))
+        member.id?.let { RequestUtils.setAttribute("memberId", it) }
+    }
+
+    @Test
+    @DisplayName("사용자 정보 조회")
+    fun 사용자_정보_조회() {
+        // when
+        val result = memberUseCase.getMember()
+
+        // then
+        assertThat(result.memberId).isEqualTo(RequestUtils.getAttribute("memberId").toLong())
+        assertThat(result.email).isEqualTo("email")
+        assertThat(result.gender).isEqualTo(GenderType.MALE)
+        assertThat(result.ageRange).isEqualTo("20")
+    }
+}
