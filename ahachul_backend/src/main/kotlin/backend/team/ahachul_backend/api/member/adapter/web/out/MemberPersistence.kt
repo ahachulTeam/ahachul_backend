4f87dabc9f1bc05a7b73179da8a37ff990171649
Hyun <mylifeforcoding@gmail.com>
2023-04-06 12:28:55 +0900

    :sparkles: 권한 인터셉터 구현 (#18) (#20)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/adapter/web/out/MemberPersistence.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/member/adapter/web/out/MemberPersistence.kt
@@ -3,6 +3,8 @@ package backend.team.ahachul_backend.api.member.adapter.web.out
 import backend.team.ahachul_backend.api.member.application.port.out.MemberReader
 import backend.team.ahachul_backend.api.member.application.port.out.MemberWriter
 import backend.team.ahachul_backend.api.member.domain.entity.MemberEntity
+import backend.team.ahachul_backend.common.exception.AdapterException
+import backend.team.ahachul_backend.common.response.ResponseCode
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,5 +14,10 @@ class MemberPersistence(
 
     override fun save(memberEntity: MemberEntity): Long? {
         return memberRepository.save(memberEntity).id
+    }
+
+    override fun getMember(memberId: Long): MemberEntity{
+        return memberRepository.findById(memberId)
+                .orElseThrow { throw AdapterException(ResponseCode.INVALID_DOMAIN) }
     }
 }
