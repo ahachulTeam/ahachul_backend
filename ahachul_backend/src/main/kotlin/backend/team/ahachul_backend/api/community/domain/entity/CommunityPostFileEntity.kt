f404399b7e323152b540c13a478cac8fa1385303
Hyun <mylifeforcoding@gmail.com>
2023-04-18 23:55:48 +0900

    :sparkles: 커뮤니티 관련 엔티티 생성 (#55) (#56)

---
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/domain/entity/CommunityPostFileEntity.kt
@@ -0,0 +1,20 @@
+package backend.team.ahachul_backend.api.community.domain.entity
+
+import backend.team.ahachul_backend.common.domain.entity.FileEntity
+import backend.team.ahachul_backend.common.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+class CommunityPostFileEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "community_category_file_id")
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var commentPost: CommunityPostEntity,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var file: FileEntity
+): BaseEntity() {
+}
