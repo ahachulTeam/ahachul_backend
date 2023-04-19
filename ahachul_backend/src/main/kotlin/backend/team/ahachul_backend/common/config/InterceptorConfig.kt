add4fc5c725be279d22df8f20552d2e8b4e887c8
semi-cloud <sksmsrkdtpal@naver.com>
2023-04-19 17:35:03 +0900

    :sparkles: CORS 설정 추가 (#58)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/config/InterceptorConfig.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/config/InterceptorConfig.kt
@@ -2,6 +2,7 @@ package backend.team.ahachul_backend.common.config
 
 import backend.team.ahachul_backend.common.interceptor.AuthenticationInterceptor
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -14,5 +15,14 @@ class InterceptorConfig(
         registry.addInterceptor(authenticationInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/oauth/**")
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
     }
 }
