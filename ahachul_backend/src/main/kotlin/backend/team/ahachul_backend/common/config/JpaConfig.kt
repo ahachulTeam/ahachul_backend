281c34e89274466373e6a0f04d9a4baff0e468fe
semi-cloud <sksmsrkdtpal@naver.com>
2023-04-24 09:18:19 +0900

    :recycle: 상수명 변경 (#71)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/config/JpaConfig.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/config/JpaConfig.kt
@@ -8,17 +8,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import java.util.*
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 class JpaConfig {
 
     companion object {
-        private const val KEY = "memberId"
+        private const val MEMBER_ID = "memberId"
         private const val UNAUTHORIZED_VALUE = "SYSTEM"
     }
 
     @Bean
     fun auditorProvider() = AuditorAware {
-        val memberId: String = RequestUtils.getAttribute(KEY) ?: UNAUTHORIZED_VALUE
+        val memberId: String = RequestUtils.getAttribute(MEMBER_ID) ?: UNAUTHORIZED_VALUE
         Optional.of(memberId)
     }
 }
