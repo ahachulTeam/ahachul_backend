5e1e4c2e83db9d059c80b19ded837d63cf5c4305
semi-cloud <sksmsrkdtpal@naver.com>
2023-05-04 14:47:52 +0900

    :sparkles: 카테고리와 지하철 호선 엔티티 생성 (#75)

---
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/lost/domain/entity/SubwayLine.kt
@@ -0,0 +1,20 @@
+package backend.team.ahachul_backend.api.lost.domain.entity
+
+import backend.team.ahachul_backend.common.entity.BaseEntity
+import backend.team.ahachul_backend.common.model.RegionType
+import jakarta.persistence.*
+
+@Entity
+class SubwayLine (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subway_line_id")
+    var id: Long,
+
+    var name: String,
+
+    var regionType: RegionType
+
+): BaseEntity() {
+}
