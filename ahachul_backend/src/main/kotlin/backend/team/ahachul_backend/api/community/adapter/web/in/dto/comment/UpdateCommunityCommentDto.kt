1b810f7d6cbbedf6d5da06b3a22c09e88d60a1f5
Hyun <mylifeforcoding@gmail.com>
2023-05-05 01:00:39 +0900

    ✨ 커뮤니티 댓글 CRUD 구현 (#77) (#87)
    * :sparkles: 커뮤니티 코멘트 뼈대 구현 (#77)
    
    * :sparkles: 커뮤니티 코멘트 작성 구현 (#77)
    
    * :sparkles: 커뮤니티 코멘트 수정 구현 (#77)
    
    * :sparkles: 커뮤니티 코멘트 상태 컬럼 누락 추가 (#77)
    
    * :sparkles: 커뮤니티 코멘트 삭제 구현 (#77)
    
    * :memo: 커뮤니티 문서 수정 (#77)
    
    * :sparkles: 커뮤니티 코멘트 엔티티 유저 컬럼 누락 추가 (#77)
    
    * :sparkles: 커뮤니티 코멘트 조회 구현 (#77)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/adapter/web/in/dto/comment/UpdateCommunityCommentDto.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/adapter/web/in/dto/comment/UpdateCommunityCommentDto.kt
@@ -1,13 +1,31 @@
 package backend.team.ahachul_backend.api.community.adapter.web.`in`.dto.comment
 
+import backend.team.ahachul_backend.api.community.domain.entity.CommunityCommentEntity
+
 class UpdateCommunityCommentDto {
 
     data class Request(
         val content: String,
-    )
+    ) {
+        fun toCommand(id: Long): UpdateCommunityCommentCommand {
+            return UpdateCommunityCommentCommand(
+                id = id,
+                content = content
+            )
+        }
+    }
 
     data class Response(
         val id: Long,
         val content: String,
-    )
+    ) {
+        companion object {
+            fun from(entity: CommunityCommentEntity): Response {
+                return Response(
+                    id = entity.id,
+                    content = entity.content
+                )
+            }
+        }
+    }
 }
--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/application/service/CommunityCommentService.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/application/service/CommunityCommentService.kt
@@ -1,28 +1,66 @@
 package backend.team.ahachul_backend.api.community.application.service
 
-import backend.team.ahachul_backend.api.community.adapter.web.`in`.dto.comment.CreateCommunityCommentDto
-import backend.team.ahachul_backend.api.community.adapter.web.`in`.dto.comment.DeleteCommunityCommentDto
-import backend.team.ahachul_backend.api.community.adapter.web.`in`.dto.comment.GetCommunityCommentsDto
-import backend.team.ahachul_backend.api.community.adapter.web.`in`.dto.comment.UpdateCommunityCommentDto
+import backend.team.ahachul_backend.api.community.adapter.web.`in`.dto.comment.*
 import backend.team.ahachul_backend.api.community.application.port.`in`.CommunityCommentUseCase
+import backend.team.ahachul_backend.api.community.application.port.out.CommunityCommentReader
+import backend.team.ahachul_backend.api.community.application.port.out.CommunityCommentWriter
+import backend.team.ahachul_backend.api.community.application.port.out.CommunityPostReader
+import backend.team.ahachul_backend.api.community.domain.entity.CommunityCommentEntity
+import backend.team.ahachul_backend.api.member.application.port.out.MemberReader
+import backend.team.ahachul_backend.common.utils.RequestUtils
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class CommunityCommentService: CommunityCommentUseCase {
+@Transactional(readOnly = true)
+class CommunityCommentService(
+    private val communityCommentWriter: CommunityCommentWriter,
+    private val communityCommentReader: CommunityCommentReader,
+    private val communityPostReader: CommunityPostReader,
+    private val memberReader: MemberReader,
+): CommunityCommentUseCase {
 
-    override fun getCommunityComments(): GetCommunityCommentsDto.Response {
-        TODO("Not yet implemented")
+    override fun getCommunityComments(command: GetCommunityCommentsCommand): GetCommunityCommentsDto.Response {
+        val comments = communityCommentReader.findAllByPostId(command.postId)
+            .map {
+                GetCommunityCommentsDto.CommunityComment(
+                    it.id,
+                    it.upperCommunityComment?.id,
+                    it.content,
+                    it.createdAt,
+                    it.createdBy,
+                    it.member.nickname!!
+                )
+            }
+            .toList()
+        return GetCommunityCommentsDto.Response(comments)
     }
 
-    override fun createCommunityComment(): CreateCommunityCommentDto.Response {
-        TODO("Not yet implemented")
+    @Transactional
+    override fun createCommunityComment(command: CreateCommunityCommentCommand): CreateCommunityCommentDto.Response {
+        val memberId = RequestUtils.getAttribute("memberId")!!
+        val upperCommunityComment = command.upperCommentId?.let { communityCommentReader.findById(it) }
+        val communityPost = communityPostReader.getCommunityPost(command.postId)
+        val member = memberReader.getMember(memberId.toLong())
+        val entity = communityCommentWriter.save(CommunityCommentEntity.of(command, upperCommunityComment, communityPost, member))
+        return CreateCommunityCommentDto.Response.from(entity)
     }
 
-    override fun updateCommunityComment(): UpdateCommunityCommentDto.Response {
-        TODO("Not yet implemented")
+    @Transactional
+    override fun updateCommunityComment(command: UpdateCommunityCommentCommand): UpdateCommunityCommentDto.Response {
+        val memberId = RequestUtils.getAttribute("memberId")!!
+        val communityComment = communityCommentReader.getById(command.id)
+        communityComment.checkMe(memberId)
+        communityComment.update(command.content)
+        return UpdateCommunityCommentDto.Response.from(communityComment)
     }
 
-    override fun deleteCommunityComment(): DeleteCommunityCommentDto.Response {
-        TODO("Not yet implemented")
+    @Transactional
+    override fun deleteCommunityComment(command: DeleteCommunityCommentCommand): DeleteCommunityCommentDto.Response {
+        val memberId = RequestUtils.getAttribute("memberId")!!
+        val communityComment = communityCommentReader.getById(command.id)
+        communityComment.checkMe(memberId)
+        communityComment.delete()
+        return DeleteCommunityCommentDto.Response(communityComment.id)
     }
 }
