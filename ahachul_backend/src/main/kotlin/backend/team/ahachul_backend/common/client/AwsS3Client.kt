6af12f8dfe98f454771c1760bdb32b52598dc2fc
Joohyun Ha <mylifeforcoding@gmail.com>
2023-05-08 19:35:04 +0900

    :sparkles: convertToFile 함수 수정, 여러 건 파일 업로드 구현 (#88)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/client/AwsS3Client.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/common/client/AwsS3Client.kt
@@ -1,8 +1,12 @@
 package backend.team.ahachul_backend.common.client
 
+import backend.team.ahachul_backend.common.exception.CommonException
+import backend.team.ahachul_backend.common.logging.Logger
 import backend.team.ahachul_backend.common.properties.AwsS3Properties
+import backend.team.ahachul_backend.common.response.ResponseCode
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.PutObjectRequest
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
@@ -15,14 +19,24 @@ class AwsS3Client(
     private val s3Properties: AwsS3Properties
 ) {
 
+    val logger: Logger = Logger(javaClass)
+
     fun upload(file: MultipartFile) {
-        s3Client.putObject(
-            PutObjectRequest(
-                s3Properties.bucketName,
-                generateRandomUUID(),
-                convertToFile(file)
+        try {
+            s3Client.putObject(
+                PutObjectRequest(
+                    s3Properties.bucketName,
+                    generateRandomUUID(),
+                    convertToFile(file)
+                )
             )
-        )
+        } catch (e: CommonException) {
+            logger.error(e.message, ResponseCode.BAD_REQUEST, e.stackTraceToString())
+        }
+    }
+
+    fun upload(files: List<MultipartFile>) {
+        files.forEach { upload(it) }
     }
 
     private fun generateRandomUUID(): String {
@@ -30,11 +44,12 @@ class AwsS3Client(
     }
 
     private fun convertToFile(multipartFile: MultipartFile): File {
-        val file = File(multipartFile.originalFilename!!)
-        file.createNewFile()
-        val fos = FileOutputStream(file)
-        fos.write(multipartFile.bytes)
-        fos.close()
-        return file
+        val tempFile = File.createTempFile(multipartFile.originalFilename!!, "")
+        multipartFile.inputStream.use { input ->
+            tempFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+        return tempFile
     }
 }
