602434d371f51f4f189a47e876e784914bf90c9d
Joohyun Ha <mylifeforcoding@gmail.com>
2023-05-10 16:03:55 +0900

    :sparkles: redisclient 동작 방식 변경 (#99)

--- ahachul_backend/src/test/kotlin/backend/team/ahachul_backend/common/client/RedisClientTest.kt
+++ ahachul_backend/src/test/kotlin/backend/team/ahachul_backend/common/client/RedisClientTest.kt
@@ -3,7 +3,6 @@ package backend.team.ahachul_backend.common.client
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -12,13 +11,40 @@ class RedisClientTest(
     @Autowired val redisClient: RedisClient
 ) {
 
-    @Test
+    private val test = "TEST"
+
+//    @Test TODO
     @DisplayName("레디스 기본동작 테스트")
     fun 레디스_기본동작_테스트() {
-        val test = "TEST"
+        // when, then
         redisClient.set(test, test)
         assertThat(redisClient.get(test)).isEqualTo(test)
         redisClient.delete(test)
         assertThat(redisClient.get(test)).isNull()
     }
+
+//    @Test
+    @DisplayName("레디스 객체_기본동작 테스트")
+    fun 레디스_객체_기본동작_테스트() {
+        val testMock = TestMock(
+            a = "a",
+            b = true,
+            c = 1234
+        )
+
+        // when
+        redisClient.set(test, testMock)
+        val result = redisClient.get(test, TestMock::class.java)!!
+
+        // then
+        assertThat(result.a).isEqualTo("a")
+        assertThat(result.b).isEqualTo(true)
+        assertThat(result.c).isEqualTo(1234)
+    }
+
+    data class TestMock(
+        val a: String,
+        val b: Boolean,
+        val c: Int
+    )
 }
