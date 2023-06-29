f861aad4f7197e53a3d8c2825d119cbf8b3e9be9
Joohyun Ha <mylifeforcoding@gmail.com>
2023-06-29 20:43:47 +0900

    :wrench: modify .sql

---
+++ ahachul_backend/src/main/resources/db/migration/V202306291857__update_subway_line.sql
@@ -0,0 +1,11 @@
+ALTER TABLE tb_subway_line
+    MODIFY created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE tb_subway_line
+    MODIFY updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE tb_subway_line
+    MODIFY created_by VARCHAR(50) DEFAULT 'SYSTEM';
+
+ALTER TABLE tb_subway_line
+    MODIFY updated_by VARCHAR(50) DEFAULT 'SYSTEM';
