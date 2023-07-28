c57310ffbbc8ace2323c04cbe173d641879c4e53
semi-cloud <sksmsrkdtpal@naver.com>
2023-07-28 23:32:37 +0900

    :sparkles: 카테고리 시드 파일 생성 (#136)

--- ahachul_backend/src/main/resources/db/seed/R__category_seed.sql
+++ ahachul_backend/src/main/resources/db/seed/R__category_seed.sql
@@ -1,15 +1,3 @@
-INSERT INTO tb_member(nickname, provider_user_id, provider, region_type, email, gender, age_range) values('세미', '22012412', 'KAKAO', 'METROPOLITAN', 'ss@naver.com', 'MALE', '20');
-
-INSERT INTO tb_subway_line(name, region_type) values('1호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('2호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('3호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('4호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('5호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('6호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('7호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('8호선', 'METROPOLITAN');
-INSERT INTO tb_subway_line(name, region_type) values('9호선', 'METROPOLITAN');
-
 INSERT IGNORE INTO tb_category(name) values('가방');
 INSERT IGNORE INTO tb_category(name) values('귀금속');
 INSERT IGNORE INTO tb_category(name) values('도서용품');
