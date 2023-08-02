5627f22409d83ea901bdbd6473d3d46da16fa935
Joohyun Ha <mylifeforcoding@gmail.com>
2023-08-02 21:01:26 +0900

    :sparkles: 커뮤니티 게시글 다수 건 조회 시 핫 게시물 여부 필드 추가 (#138)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/domain/SearchCommunityPost.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/community/domain/SearchCommunityPost.kt
@@ -2,6 +2,7 @@ package backend.team.ahachul_backend.api.community.domain
 
 import backend.team.ahachul_backend.api.community.domain.model.CommunityCategoryType
 import backend.team.ahachul_backend.common.model.RegionType
+import backend.team.ahachul_backend.common.model.YNType
 import java.time.LocalDateTime
 
 data class SearchCommunityPost(
@@ -13,6 +14,7 @@ data class SearchCommunityPost(
     val subwayLineId: Long,
     val likeCnt: Long,
     val commentCnt: Long,
+    val hotPostYn: YNType,
     val createdAt: LocalDateTime,
     val createdBy: String,
     val writer: String,
