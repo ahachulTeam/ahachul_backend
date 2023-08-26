f72c05862f0ae6b573b3079c7dc2871a6014c2c1
semi-cloud <sksmsrkdtpal@naver.com>
2023-08-26 18:31:42 +0900

    :sparkles: @EnableAsync 추가 (#158)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/AhachulBackendApplication.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/AhachulBackendApplication.kt
@@ -3,9 +3,11 @@ package backend.team.ahachul_backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableAsync
 class AhachulBackendApplication
 
 fun main(args: Array<String>) {
