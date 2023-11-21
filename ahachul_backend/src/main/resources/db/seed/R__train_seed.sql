25b9be7ac8a82debbca57eba8352d38edbf8613b
Joohyun Ha <mylifeforcoding@gmail.com>
2023-11-21 21:40:24 +0900

    ♻️ data.sql 정류장, 노선 정보 추가 및 정리 (#189) (#193)
    * :recycle: data.sql 바뀐 지하철 노선 데이터대로 지하철 번호 데이터 수정 (#189)
    
    * refactor: db seed 파일 수정 (#189)
    
    ---------
    
    Co-authored-by: Semi kang <71436576+semi-cloud@users.noreply.github.com>

--- ahachul_backend/src/main/resources/db/seed/R__train_seed.sql
+++ ahachul_backend/src/main/resources/db/seed/R__train_seed.sql
@@ -1,54 +1,48 @@
-# 1호선
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('0', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('K', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('S', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('1', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('311', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('312', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('319', 1);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('341', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('0', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('K', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('S', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('1', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('311', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('312', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('319', 1);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('341', 1);
 
-# 2 ~ 9호선
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('2', 2);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('3', 3);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('4', 4);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('5', 5);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('6', 6);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('7', 7);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('8', 8);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('9', 9);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('2', 2);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('3', 3);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('4', 4);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('5', 5);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('6', 6);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('7', 7);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('8', 8);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('9', 9);
 
-# 신분당선
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D01', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D02', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D03', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D04', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D05', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D06', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D07', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D08', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D09', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D10', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D11', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D12', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D13', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D14', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D15', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D16', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D17', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D18', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D19', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D20', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D21', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D22', 10);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('D23', 10);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D01', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D02', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D03', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D04', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D05', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D06', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D07', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D08', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D09', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D10', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D11', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D12', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D13', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D14', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D15', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D16', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D17', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D18', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D19', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D20', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D21', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D22', 18);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('D23', 18);
 
-# 수인분당선
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('351', 11);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('351', 16);
 
-# 경의중앙선
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('321', 12);
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('331', 12);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('321', 11);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('331', 11);
 
-# 우이신설선
-INSERT IGNORE INTO tb_train(prefix_train_no, subway_line_id) values('UL', 13);
+INSERT INTO tb_train(prefix_train_no, subway_line_id) values('UL', 20);
