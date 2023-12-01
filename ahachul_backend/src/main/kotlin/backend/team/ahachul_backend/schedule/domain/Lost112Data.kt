504fa1aec9507940f0e61df9b59be3dd8290dee0
semi-cloud <sksmsrkdtpal@naver.com>
2023-12-01 15:57:25 +0900

    :recycle: 수집 데이터에 Lost112 ID 필드 추가

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/schedule/domain/Lost112Data.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/schedule/domain/Lost112Data.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 
 data class Lost112Data(
+    @SerializedName("ID")
+    val id: String = "",
     val title: String = "",
     val getDate: String = "",
     val getPlace: String = "",
