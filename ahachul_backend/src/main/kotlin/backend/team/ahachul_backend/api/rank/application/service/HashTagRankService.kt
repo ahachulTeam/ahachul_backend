33f5fa5d70d3dea9eefa28213abbd608ae6a34e1
semi-cloud <sksmsrkdtpal@naver.com>
2024-01-10 17:16:19 +0900

    :sparkles: 해시태그 로그 파일 저장 구현 (#204)

--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/rank/application/service/HashTagRankService.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/rank/application/service/HashTagRankService.kt
@@ -1,34 +1,98 @@
 package backend.team.ahachul_backend.api.rank.application.service
 
-import backend.team.ahachul_backend.api.rank.adapter.web.`in`.dto.GetHashTagRankDto
 import backend.team.ahachul_backend.common.client.RedisClient
+import backend.team.ahachul_backend.common.exception.CommonException
+import backend.team.ahachul_backend.common.logging.Logger
+import backend.team.ahachul_backend.common.response.ResponseCode
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import org.springframework.stereotype.Service
+import java.io.BufferedWriter
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 @Service
 class HashTagRankService(
-    val redisClient: RedisClient
+    val redisClient: RedisClient,
 ){
+    var buffer = mutableListOf<JsonObject>()
+    val logger = Logger(javaClass)
 
-    fun increaseCount(hashTagName: String) {
-        val setOperations = redisClient.getZSetOps()
-        setOperations.incrementScore(KEY, hashTagName, INCREASE_CNT.toDouble())
+    /**
+     * 비동기로 해시태그 로그 파일에 정보를 저장한다.
+     */
+    fun saveLog(name: String, userId: String, localDateTime: LocalDateTime) {
+        val hashtagFileUrl = BASE_DIR + "${localDateTime.year}_${localDateTime.monthValue}_hashtag_log"
+
+        // 저장 날짜에 해당하는 날짜 파일이 없다면 생성
+        createFile(hashtagFileUrl)
+
+        // 임시 버퍼에 들어오는 데이터 저장
+        val jsonObject = createJsonObject(name, userId, localDateTime)
+        if (buffer.size >= MAX_BUFFER_SIZE) {
+            saveFile(hashtagFileUrl, buffer)
+            buffer = mutableListOf()
+        }
+        buffer.add(jsonObject)
     }
 
-    fun getRank(): GetHashTagRankDto.Response {
-        val setOperations = redisClient.getZSetOps()
-        val responseSet = setOperations.reverseRange(KEY, 0, LIMIT)
-        val response = responseSet?.toList() ?: listOf()
-        return GetHashTagRankDto.Response(response)
+    private fun createFile(fileUrl: String) {
+        val path = Path.of(fileUrl).normalize()
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path)
+            }
+        } catch (ex: IOException) {
+            throw CommonException(ResponseCode.FILE_CREATE_FAILED, ex)
+        }
     }
 
-    fun get(hashTagName: String): Double? {
-        val setOperations = redisClient.getZSetOps()
-        return setOperations.score(KEY, hashTagName)
+    private fun createJsonObject(name: String, userId: String, localDateTime: LocalDateTime): JsonObject {
+        val currentTime = localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"))
+        val jsonObject = JsonObject()
+        jsonObject.addProperty("name", name)
+        jsonObject.addProperty("userId", userId)
+        jsonObject.addProperty("timestamp", currentTime)
+        return jsonObject
+    }
+
+    private fun saveFile(filePath: String, data: List<Any>) {
+        val gson = Gson()
+        logger.info("save hashtag log in to file : $filePath")
+        try {
+            BufferedWriter(OutputStreamWriter(FileOutputStream(filePath, true), "UTF-8")).use {
+                data
+                    .map { data -> gson.toJson(data) }
+                    .forEach { str ->
+                        it.write(str)
+                        it.newLine()
+                    }
+            }
+        } catch (ex: IOException) {
+            throw CommonException(ResponseCode.FILE_WRITE_FAILED, ex)
+        }
+    }
+
+    /**
+     * 캐시해둔 순위를 조회한다.
+     */
+    fun getRank(): List<String> {
+        val mapper = ObjectMapper()
+        val typeRef = object : TypeReference<List<String>>() {}
+        return mapper.readValue(redisClient.get("hashtag_rank"), typeRef)
     }
 
     companion object {
-        const val KEY = "hashtags"
-        const val INCREASE_CNT = 1
-        const val LIMIT = 10L
+        const val MAX_BUFFER_SIZE = 100
+        // TODO S3에 파일 업로드
+        const val BASE_DIR = "/Users/kangsemi/Desktop/git/ahachul_backend/ahachul_backend/src/main/resources/"
     }
 }
--- ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/rank/event/HashTagSearchEvent.kt
+++ ahachul_backend/src/main/kotlin/backend/team/ahachul_backend/api/rank/event/HashTagSearchEvent.kt
@@ -1,6 +1,10 @@
 package backend.team.ahachul_backend.api.rank.event
 
+import java.time.LocalDateTime
+
 data class HashTagSearchEvent(
-    val hashTagName: String
+    val hashTagName: String,
+    val userId: String,
+    val timestamp: LocalDateTime
 ) {
 }
